6cd765da744364a5db7d0a862a87cf1174045c5a
mr-yragan <kondratyev120307@gmail.com>
2025-03-13 21:23:40 +0300

    CONSTS

---
+++ GameRoom.tsx
@@ -0,0 +1,5 @@
+const GameRoom = () => {
+    const { roomId } = useParams<{ roomId: string }>();
+    const navigate = useNavigate();
+    const [gameRoom, setGameRoom] = useState<GameRoomType | null>(null);
+    const [currentUser, setCurrentUser] = useState<User | null>(null);п
